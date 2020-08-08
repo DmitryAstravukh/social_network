4cc9f01bbd19a0ff3a5e50e12518490682f44de8
MrFreeman <astrauh1.dima1@ya.ru>
2020-08-08 11:29:32 +0300

    reg component, adaptive

--- src/pages/login/login-page.jsx
+++ src/pages/login/login-page.jsx
@@ -6,6 +6,64 @@ import { faUserPlus, faKey, faUser, faSignInAlt } from '@fortawesome/free-solid-
 
 import logo from './../../assets/image/white-logo.svg';
 
+const SigninContent = () => {
+  return(
+    <div className='login-page__content-signin'>
+      <h1>Добро пожаловать</h1>
+      <p>Войдите, чтобы общаться и делиться новостями</p>
+      <form action='' className='login-form'>
+        <div className='input-group'>
+          <div className='input-group__email-signin'>
+            <input type='email' placeholder='Email'/>
+            <FontAwesomeIcon icon={faUser} />
+          </div>
+          <hr/>
+          <div className='input-group__pass-signin'>
+            <input type='password' placeholder='Пароль'/>
+            <FontAwesomeIcon icon={faKey} />
+          </div> 
+        </div>
+        
+        <div className='additional-functions'>
+          <div className='remember-login'>
+            <input type='checkbox' 
+                    className='remember-login__checkbox'
+                    id='remember-login__checkbox' />
+            <label htmlFor='remember-login__checkbox'>Запомнить меня</label>
+          </div>
+          <a href='/' className='lost-password'>Забыли пароль?</a>
+        </div> 
+        
+        <button className='login-btn-signin'>Войти</button>
+      </form>
+    </div>
+  )
+}
+
+const SignupContent = () => {
+  return(
+    <div className='login-page__content-signup'>
+      <h1>Добро пожаловать</h1>
+      <p>Зарегистрируйтесь, чтобы общаться и делиться новостями</p>
+      <form action='' className='login-form'>
+        <div className='input-group'>
+          <div className='input-group__email-signup'>
+            <input type='email' placeholder='Email или логин'/>
+            <FontAwesomeIcon icon={faUser} />
+          </div>
+          <hr/>
+          <div className='input-group__pass-signup'>
+            <input type='password' placeholder='Пароль'/>
+            <FontAwesomeIcon icon={faKey} />
+          </div> 
+        </div>
+        
+        <button className='login-btn-signup'>Зарегистрироваться</button>
+      </form>
+    </div>
+  )
+}
+
 export default class LoginPage extends Component{
   render(){
     return (
@@ -29,36 +87,9 @@ export default class LoginPage extends Component{
             </div>
           </div>
           
-          <div className='login-page__content'>
-            <h1>Добро пожаловать</h1>
-            <p>Войдите, чтобы общаться и делиться новостями</p>
-            <form action='' className='login-form'>
-              <div className='input-group'>
-                <div className='input-group__email'>
-                  <input type='email' placeholder='Email или логин'/>
-                  <FontAwesomeIcon icon={faUser} />
-                </div>
-                <hr/>
-                <div className='input-group__pass'>
-                  <input type='password' placeholder='Пароль'/>
-                  <FontAwesomeIcon icon={faKey} />
-                </div> 
-              </div>
-              
-              <div className='additional-functions'>
-                <div className='remember-login'>
-                  <input type='checkbox' 
-                         className='remember-login__checkbox'
-                         id='remember-login__checkbox' />
-                  <label htmlFor='remember-login__checkbox'>Запомнить меня</label>
-                </div>
-                <a href='/' className='lost-password'>Забыли пароль?</a>
-              </div> 
-              
-              <button className='login-btn'>Войти</button>
-            </form>
-            
-          </div>
+          <SigninContent />
+          {/* <SignupContent /> */}
+
         </div>
           
       </div>

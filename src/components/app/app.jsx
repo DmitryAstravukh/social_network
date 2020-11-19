200551cab139f243d11a1cc686ca33ad405a4f33
MrFreeman <astrauh1.dima1@ya.ru>
2020-11-19 19:24:39 +0300

    add lazy import to component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,18 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Suspense } from 'react';
 import './app.scss';
 import { useDispatch, useSelector } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
-import { getAuthUserData } from './../../reducers/auth';
-
-import Login from '../login';
-import Header from '../header';
-import Navbar from '../navbar';
-import News from '../news';
-import Messages from '../messages';
-import { UsersList } from '../users-list/users-list';
-import { UserProfile } from '../user-profile/user-profile';
 import { inicializeApp } from '../../reducers/inicialize';
 import Spiner from '../spiner';
+
+const Login = React.lazy(() => import('../login'));
+const Header = React.lazy(() => import('../header'));
+const Navbar = React.lazy(() => import('../navbar'));
+const UsersList = React.lazy(() => import('../users-list'));
+const UserProfile = React.lazy(() => import('../user-profile'));
+const News = React.lazy(() => import('../news'));
+const Messages = React.lazy(() => import('../messages'));
 
 const App = () => {
     const dispatch = useDispatch();
@@ -24,19 +23,21 @@ const App = () => {
     if(!isInicialized) return <Spiner />
     return (
         <div className='app'>
-            <Header />
-            <div className='content'>
-                <Navbar />
-                <Switch>
-                    <Route exact path='/'><UsersList/></Route>
-                    <Route path='/news'><News/></Route>
-                    <Route path='/messages'><Messages/></Route>
-                    <Route path='/users'><UsersList/></Route>
-                    <Route path='/profile/:userId'><UserProfile/></Route>
-                    <Route path='/login'><Login/></Route>
-                    <Route path='*'><h1>404 Not Found</h1></Route>
-                </Switch>
-            </div>
+            <Suspense fallback={Spiner}>
+                <Header />
+                <div className='content'>
+                    <Navbar />
+                    <Switch>
+                        <Route exact path='/' component={UserProfile} />
+                        <Route path='/news' component={News} />
+                        <Route path='/messages' component={Messages} />
+                        <Route path='/users' component={UsersList} />
+                        <Route path='/profile/:userId' component={UserProfile} />
+                        <Route path='/login' component={Login} />
+                        <Route path='*'><h1>404 Not Found</h1></Route>
+                    </Switch>
+                </div>
+            </Suspense>
         </div>
     )
 }
